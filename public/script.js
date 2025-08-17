a606b8f1ffb2945d6bfe44ebdcea27cb6ee726b2
Renzy Antonio <renzy.antonio0112@gmail.com>
2025-08-17 22:35:19 +0800

    Update: Created DOM Access

---
+++ public/script.js
@@ -0,0 +1,13 @@
+const inputEl = document.getElementById('input-el');
+const searchBtn = document.getElementById('btnsearch-el');
+
+const locationEl = document.getElementById('location-el');
+const imgEl = document.getElementById('weatherimg-el');
+const celciusEl = document.getElementById('celcius-el');
+const descriptionEl = document.getElementById('description-el');
+const refreshBtn = document.getElementById('btnrefresh-el');
+
+const windEl = document.getElementById('wind-el');
+const humidEl = document.getElementById('humidity-el');
+const rainEl = document.getElementById('rain-el');
+
